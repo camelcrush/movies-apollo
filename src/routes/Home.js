81c67180ed576099fc60420a914701d68360367d
camelcrush <dkxm0927@naver.com>
2021-11-14 20:21:46 +0900

    GET Query: gql, useQuery() from @apollo/client

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const { loading, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h1>{movie.id}</h1>);
+  }
 };
 
 export default Home;
