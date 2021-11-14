e54fafe492c26003b378c7210d7ff5ca26a70a12
camelcrush <dkxm0927@naver.com>
2021-11-14 21:01:06 +0900

    Detail: useParams(), parseInt(id)

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
 
 const Detail = () => {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return <h1>{data.movie.title}</h1>;
+  }
 };
 
 export default Detail;
