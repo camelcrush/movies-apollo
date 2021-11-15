2aae6e29649d6cd158fb2bc1c1cf5ceeca774096
camelcrush <dkxm0927@naver.com>
2021-11-15 22:12:36 +0900

    Local State: Mutation & cache.modify() for toggle like

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { gql, useMutation } from "@apollo/client";
+
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!) {
+    likeMovie(id: $id) @client
+  }
+`;
 
 const Container = styled.div`
   width: 100%;
-  height: 480px;
+  height: 400px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
   border-radius: 7px;
+  background-color: transparent;
 `;
 
 const Poster = styled.div`
@@ -19,12 +26,15 @@ const Poster = styled.div`
 `;
 
 const Movie = ({ id, bg, isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
   return (
     <Container>
       <Link to={`/${id}`}>
         <Poster bg={bg} />
       </Link>
-      <button>{isLiked ? "Unlike" : "Like"}</button>
+      <button onClick={likeMovie}>{isLiked ? "Unlike" : "Like"}</button>
     </Container>
   );
 };
